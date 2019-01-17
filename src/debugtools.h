1203ba7e165bc113858d2ae190b2a3d95abb343a
Allan Cameron <CAMERAL931@xggc.scot.nhs.uk>
2019-01-17 17:08:49 +0000

    further comments

--- src/debugtools.h
+++ src/debugtools.h
@@ -54,29 +54,35 @@ void printvec(std::vector<T> x)
   size_t s = x.size();
   // In the special case of an empty vector, make this explicit
   if(s == 0) std::cout << "[empty vector]" << std::endl;
+  // print the contents of the vector followed by a comma for all but last
   if(s > 1) for(size_t i = 0; i < (s - 1); i++) std::cout << x[i] << ", ";
+  // the last entry doesn't need a comma after it
   if(s > 0) std::cout << x[s - 1] << std::endl;
 }
 
 /*---------------------------------------------------------------------------*/
+// Simple inline function to start a timer from which subsequent time points
+// can be measured. Needed for profiling.
 
 inline std::chrono::high_resolution_clock::time_point startClock()
 {
-  return std::chrono::high_resolution_clock::now();
+  return std::chrono::high_resolution_clock::now(); // returns current time
 }
 
 /*---------------------------------------------------------------------------*/
+// Measures and prints out the time in milliseconds since the timer was
+// started for use in profiling.
 
 inline void timeSince(std::string message,
                std::chrono::high_resolution_clock::time_point& start)
 {
   std::chrono::high_resolution_clock::time_point end =
-    std::chrono::high_resolution_clock::now();
-  auto diff = end - start;
+    std::chrono::high_resolution_clock::now(); // gets current time
+  auto diff = end - start; // diff now contains time since clock was started
   std::cout << message << ": "
             << std::chrono::duration <double, std::milli> (diff).count()
-            << " ms" << std::endl;
-  start = std::chrono::high_resolution_clock::now();
+            << " ms" << std::endl; //prints time in milliseconds to console
+  start = std::chrono::high_resolution_clock::now(); // resets clock
 }
 
 /*---------------------------------------------------------------------------*/
